e818cfc85afa34405de7cc4c8ac0d47035dc59e8
mohan <mohanprasath962000@gmail.com>
2023-02-25 22:43:37 +0530

    stripe added

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -1,14 +1,14 @@
-import { useState } from 'react';
-import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
-import { useSelector } from 'react-redux';
+import { useState } from "react";
+import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
+import { useSelector } from "react-redux";
 
-import { selectCartTotal } from '../../store/cart/cart.selector';
-import { selectCurrentUser } from '../../store/user/user.selector';
+import { selectCartTotal } from "../../store/cart/cart.selector";
+import { selectCurrentUser } from "../../store/user/user.selector";
 
-import { FormContainer } from './payment-form.styles';
-import { BUTTON_TYPE_CLASSES } from '../button/button.component';
+import { FormContainer } from "./payment-form.styles";
+import { BUTTON_TYPE_CLASSES } from "../button/button.component";
 
-import { PaymentButton, PaymentFormContainer } from './payment-form.styles';
+import { PaymentButton, PaymentFormContainer } from "./payment-form.styles";
 
 const PaymentForm = () => {
   const stripe = useStripe();
@@ -18,15 +18,18 @@ const PaymentForm = () => {
   const [isProcessingPayment, setIsProcessingPayment] = useState(false);
 
   const paymentHandler = async (e) => {
+    console.log("Button pressed", stripe, elements);
+
     e.preventDefault();
     if (!stripe || !elements) {
       return;
     }
+    console.log("Button pressed 2");
     setIsProcessingPayment(true);
-    const response = await fetch('/.netlify/functions/create-payment-intent', {
-      method: 'post',
+    const response = await fetch("/.netlify/functions/create-payment-intent", {
+      method: "post",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({ amount: amount * 100 }),
     }).then((res) => {
@@ -39,7 +42,7 @@ const PaymentForm = () => {
       payment_method: {
         card: elements.getElement(CardElement),
         billing_details: {
-          name: currentUser ? currentUser.displayName : 'Yihua Zhang',
+          name: currentUser ? currentUser.displayName : "Yihua Zhang",
         },
       },
     });
@@ -49,8 +52,8 @@ const PaymentForm = () => {
     if (paymentResult.error) {
       alert(paymentResult.error.message);
     } else {
-      if (paymentResult.paymentIntent.status === 'succeeded') {
-        alert('Payment Successful!');
+      if (paymentResult.paymentIntent.status === "succeeded") {
+        alert("Payment Successful!");
       }
     }
   };
